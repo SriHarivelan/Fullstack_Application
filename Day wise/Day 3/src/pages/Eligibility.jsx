54db0ecfa643c2bbd011aefb5544415195e10f12
SriHarivelan <114942830+SriHarivelan@users.noreply.github.com>
2024-02-26 14:23:23 +0530

    Add files via upload

---
+++ Day wise/Day 3/src/pages/Eligibility.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../assets/css/menu.css"
+import HomeScreen from './Home'
+
+const Eligibility = () => {
+  return (
+    <div>
+        <div className='na'>
+        <HomeScreen/>
+        </div> 
+        <div className='bo'> 
+      <h1>This is the Eligibility</h1>\</div>
+    </div>
+  )
+}
+
+export default Eligibility
